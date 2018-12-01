89c59c54cb59c32313b108b423065cf6fa9ce68d
Zach Mullen <zach.mullen@kitware.com>
2018-12-01 09:58:21 -0500

    Refraction

--- raytrace.py
+++ raytrace.py
@@ -23,6 +23,34 @@ def solve_quadratic(a, b, c):
     return sorted((x0, x1))
 
 
+def refract(i, n, index):
+    cosi = max(-1., min(1., i.dot(n)))  # clamp due to precision loss
+    etai = 1
+    etat = index
+
+    if cosi < 0:
+        cosi = -cosi
+    else:
+        etai, etat = etat, etai
+        n = -n
+
+    eta = etai / etat
+    k = 1. - eta * eta * (1. - cosi * cosi);
+    if k < 0:
+        return None
+    else:
+        return eta * i + (eta * cosi - math.sqrt(k)) * n
+
+
+class Shape(object):
+    def refract(self, ray, p):
+        n = self.surface_normal(p)
+        refract_dir = refract(ray.dir, n, self.mat.r_index)
+        if refract_dir is None:
+            return None
+        return Ray(p + EPSILON * refract_dir, refract_dir)
+
+
 class PointLight(object):
     __slots__ = ['pos', 'intensity']
 
@@ -60,18 +88,19 @@ class Ray(object):
 
 
 class Material(object):
-    __slots__ = ['amb', 'diff', 'reflect', 'refract', 'color']
+    __slots__ = ['color', 'amb', 'diff', 'reflect', 'refract', 'r_index']
 
-    def __init__(self, color, amb, diff, reflect, refract=0.):
+    def __init__(self, color, amb, diff, reflect, refract=0., r_index=1):
         self.color = color
         self.amb = amb
         # self.spec = spec
         self.diff = diff
         self.reflect = reflect
         self.refract = refract
+        self.r_index = r_index
 
 
-class Sphere(object):
+class Sphere(Shape):
     def __init__(self, center, r, mat):
         self.center = center
         self.r_sq = r * r
@@ -101,7 +130,7 @@ class Sphere(object):
         return v / np.linalg.norm(v)
 
 
-class Plane(object):
+class Plane(Shape):
     def __init__(self, p, normal, mat):
         self.p = p
         self.normal = normal / np.linalg.norm(normal)
@@ -166,7 +195,8 @@ class Scene(object):
             return sum((
                 (obj.mat.color * obj.mat.amb),
                 self._diffuse(obj, p),
-                self._reflect(obj, ray, p, ttl)
+                self._reflect(obj, ray, p, ttl),
+                self._refract(obj, ray, p, ttl)
             ))
 
     def _diffuse(self, obj, p):
@@ -198,26 +228,40 @@ class Scene(object):
         reflect_ray = Ray(p + reflect_dir * EPSILON, reflect_dir)
         return obj.mat.reflect * self._color(reflect_ray, ttl - 1)
 
+    def _refract(self, obj, ray, p, ttl):
+        if ttl == 0 or obj.mat.refract <= 0:
+            return np.array((0, 0, 0))
+        refract_ray = obj.refract(ray, p)
+        if refract_ray is None:
+            return np.array((0, 0, 0))
+        return obj.mat.refract * self._color(refract_ray, ttl - 1)
+
 
 if __name__ == '__main__':
     s1_mat = Material(np.array((255, 50, 50)), amb=0.2, diff=0.5, reflect=0.3)
     s2_mat = Material(np.array((30, 250, 120)), amb=0.2, diff=0.5, reflect=0.3)
     s3_mat = Material(np.array((20, 20, 255)), amb=0.1, diff=0.2, reflect=0.7)
+    s4_mat = Material(
+        np.array((255, 255, 255)), amb=0, diff=0.1, reflect=0.25, refract=0.9, r_index=1.1)
     floor_mat = Material(np.array((150, 150, 240)), amb=0.2, diff=0.5, reflect=0.3)
 
     s1 = Sphere(np.array((0, -1, -6)), 1, s1_mat)
     s2 = Sphere(np.array((-4, 0.4, -8)), 2, s2_mat)
     s3 = Sphere(np.array((4, 2, -10)), 2.5, s3_mat)
+    s4 = Sphere(np.array((-1, -1, -4)), 0.8, s4_mat)
     floor = Plane(np.array((0, -2, 0)), np.array((0, 1, 0)), floor_mat)
 
-    light = PointLight(np.array((0, 0, 0)), 600)
+    light = PointLight(np.array((0, 0, -2.5)), 700)
     # light = DirectionalLight(np.array((0, -1, 0)))
 
     camera = Camera(hres=700, vres=500, fov=65)
-    scene = Scene(camera, [s1, s2, s3, floor], [light], np.array((0, 0, 0)))
+    scene = Scene(camera, [s1, s2, s3, s4, floor], [light], np.array((0, 0, 0)))
     Image.fromarray(scene.render()).save('out.png')
 
     # Next up:
-    # 1. Light color
-    # 2. Refraction
-    # 3. Textures
+    # . Specular light
+    # . Multiprocessing
+    # . Area light (disk? plane? buncha points?)
+    # . Light color
+    # . Textures
+    # . Fresnel
