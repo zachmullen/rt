326e7d53fb6355a6cdc4613ff4c1967cda176e81
Zach Mullen <zach.mullen@kitware.com>
2018-11-30 21:04:00 -0500

    Reflection

--- raytrace.py
+++ raytrace.py
@@ -159,9 +159,11 @@ class Scene(object):
             return self.bg_color
         else:
             p = ray.orig + ray.dir * min_t  # intersection with object
-            return (obj.mat.color * obj.mat.amb) + \
-                self._diffuse(obj, p)
-                #self._reflect(obj, ray, p, ttl)
+            return sum((
+                (obj.mat.color * obj.mat.amb),
+                self._diffuse(obj, p),
+                self._reflect(obj, ray, p, ttl)
+            ))
 
     def _diffuse(self, obj, p):
         if obj.mat.diff <= 0:
@@ -180,28 +182,33 @@ class Scene(object):
     def _light_or_shadow(self, ray, obj, p, light_t):  # helper for diffuse shading
         for actor in self.actors:
             t = actor.intersect(ray)
-            if t is not None and t > EPSILON and t < light_t:
+            if t is not None and EPSILON < t < light_t:
                 return np.array((0, 0, 0))
         return obj.mat.diff * obj.mat.color * obj.surface_normal(p).dot(ray.dir)
 
     def _reflect(self, obj, ray, p, ttl):
         if ttl == 0 or obj.mat.reflect <= 0:
             return np.array((0, 0, 0))
-        return np.array((0, 0, 0))  # TODO reflection, we should call _color with ttl - 1
+        n = obj.surface_normal(p)
+        reflect_dir = ray.dir - 2 * n * (ray.dir.dot(n))
+        reflect_ray = Ray(p + reflect_dir * EPSILON, reflect_dir)
+        return obj.mat.reflect * self._color(reflect_ray, ttl - 1)
 
 
 if __name__ == '__main__':
     s1_mat = Material(np.array((255, 50, 50)), amb=0.3, diff=0.5, reflect=0.2)
     s2_mat = Material(np.array((30, 250, 120)), amb=0.3, diff=0.5, reflect=0.2)
-    floor_mat = Material(np.array((150, 150, 240)), amb=0.3, diff=0.5, reflect=0.2)
+    s3_mat = Material(np.array((20, 20, 255)), amb=0.1, diff=0.2, reflect=0.5)
+    floor_mat = Material(np.array((150, 150, 240)), amb=0.2, diff=0.5, reflect=0.3)
 
     s1 = Sphere(np.array((0, -1, -6)), 1, s1_mat)
     s2 = Sphere(np.array((-4, 0, -8)), 2, s2_mat)
+    s3 = Sphere(np.array((4, 2, -10)), 2.5, s3_mat)
     floor = Plane(np.array((0, -2, 0)), np.array((0, 1, 0)), floor_mat)
 
     light = PointLight(np.array((14, 0, -4)))
     # light = DirectionalLight(np.array((0, -1, 0)))
 
-    camera = Camera(hres=400, vres=300, fov=65)
-    scene = Scene(camera, [s1, s2, floor], [light], np.array((0, 0, 0)))
+    camera = Camera(hres=640, vres=480, fov=65)
+    scene = Scene(camera, [s1, s2, s3, floor], [light], np.array((0, 0, 0)))
     Image.fromarray(scene.render()).save('out.png')
